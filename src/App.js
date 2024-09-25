80492ab798495dde6b065f1148c1ec19ad2f7891
mapadu <rajmundtakacs@gmail.com>
2024-09-25 11:59:05 +0200

    update

--- src/App.js
+++ src/App.js
@@ -8,41 +8,41 @@ import Spotify from './util/Spotify/Spotify';
 
 function App() {
 
-const [results, setResults] = useState([]);
+  const [results, setResults] = useState([]);
 
-const [playlist, setPlaylist] = useState([
-  {
-    name: 'name1',
-    artist: 'artist1',
-    album: 'album1',
-    id: 1,
-    uri: 'uri1'
-  },
-  {
-    name: 'name2',
-    artist: 'artist2',
-    album: 'album2',
-    id: 2,
-    uri: 'uri2'
+  const [playlist, setPlaylist] = useState([
+    {
+      name: 'name1',
+      artist: 'artist1',
+      album: 'album1',
+      id: 1,
+      uri: 'uri1'
+    },
+    {
+      name: 'name2',
+      artist: 'artist2',
+      album: 'album2',
+      id: 2,
+      uri: 'uri2'
+    }
+  ]);
+
+  const [trackURIs, setTrackURIs] = useState([]);
+
+  const addToPlaylist = (track) => {
+    setPlaylist([track, ...playlist])
   }
-]);
 
-const [trackURIs, setTrackURIs] = useState([]);
+  const removeFromPlaylist = (index) => {
+    setPlaylist(playlist.filter((track, i ) => i !== index))
+  }
 
-const addToPlaylist = (track) => {
-  setPlaylist([track, ...playlist])
-}
+  const savePlaylist = () => {
+    setTrackURIs(playlist.map((track) => track.uri));
+  }
 
-const removeFromPlaylist = (index) => {
-  setPlaylist(playlist.filter((track, i ) => i !== index))
-}
-
-const savePlaylist = () => {
-  setTrackURIs(playlist.map((track) => track.uri));
-}
-
-const [token, setToken] = useState('');
-const [expire, setExpire] = useState('');
+  const [token, setToken] = useState('');
+  const [expire, setExpire] = useState('');
 
 
   return (
