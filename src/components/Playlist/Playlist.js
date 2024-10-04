aa9d37083eb33e5eecbefc5f650aecc98ccd101a
mapadu <rajmundtakacs@gmail.com>
2024-10-04 14:04:47 +0200

    Update

--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -10,7 +10,7 @@ const Playlist = ({playlist, removeFromPlaylist}) => {
         <div>
             <input value={playlistName} id ='name' type='text' placeholder='Name your playlist... ' onChange={(e) => setPlaylistName(e.target.value)} />
             <div>{playlist.map((track, i) => <div key={i}>{track.artist} - {track.name}<button onClick={(event) => removeFromPlaylist(i)} >Remove</button></div>)}</div>
-            <button onClick={handleGetUserProfile}>get user profile</button>
+            <button>get user profile</button>
             <button>Save to Spotify</button>
         </div>
     )
