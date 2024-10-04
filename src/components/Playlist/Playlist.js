60137e8087cc940f1fbbb35280adb9282251365a
mapadu <rajmundtakacs@gmail.com>
2024-10-04 14:04:20 +0200

    update

--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -4,21 +4,13 @@ import React, {useState} from 'react';
 const Playlist = ({playlist, removeFromPlaylist}) => {
 
     const [playlistName, setPlaylistName] = useState('');
-
-    const clientId = '19fd446429584459855d315edf7d84fc';
-    const clientSecret = '34276136c4c64dffaf5e073c6e581416';
-    const redirectUri = 'http://localhost:3000/callback';
-
-    //const [trackURIs, setTrackURIs] = useState([]);
-    //setTrackURIs(playlist.map((track) =>  <li>{track.uri}</li>));
-
- 
+    
 
     return (
         <div>
             <input value={playlistName} id ='name' type='text' placeholder='Name your playlist... ' onChange={(e) => setPlaylistName(e.target.value)} />
             <div>{playlist.map((track, i) => <div key={i}>{track.artist} - {track.name}<button onClick={(event) => removeFromPlaylist(i)} >Remove</button></div>)}</div>
-            <button>get user profile</button>
+            <button onClick={handleGetUserProfile}>get user profile</button>
             <button>Save to Spotify</button>
         </div>
     )
